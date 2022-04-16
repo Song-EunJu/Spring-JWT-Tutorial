26847d07cfdd61b0a2de1ea6d0acaab572003254
Song-EunJu <jd06280@naver.com>
2022-04-16 12:25:45 +0900

    JWT 코드 및 Security 설정 추가

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,87 @@
+package com.example.demo.config;
+
+import com.example.demo.jwt.JwtAccessDeniedHandler;
+import com.example.demo.jwt.JwtAuthenticationEntryPoint;
+import com.example.demo.jwt.JwtSecurityConfig;
+import com.example.demo.jwt.TokenProvider;
+import io.jsonwebtoken.Jwt;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity // 기본적인 웹 보안 활성화
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메소드 단위로 추가하기 위해서
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/*",
+                        "/favicon.ico"
+                );
+        // h2-console 하위 모든 요청들과 파비콘 관련 요청은 Spring Security 로직 수행 X
+        // h2-console/** 에 대한 요청은 Security filter chain을 적용할 필요가 전혀 없는 요청이여서 ignoring 했습니다.
+        // permitAll()로 해도 동작에는 문제가 없지만 의미나 로직 상 ignoring 쪽이 더 적합하다고 생각했습니다~!
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // 토큰을 사용하기 때문에 disable
+                // exception 핸들링할 때 우리가 만든 클래스 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // h2-console 을 위한 설정
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션 사용하지 않으므로 stateless
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다
+                // /api/hello 에 대한 요청은 인증없이 접근을 허용하겠다.
+                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/singup").permitAll()
+                // 나머지 요청들은 모두 인증되어야 한다.
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+    }
+}
---
+++ src/main/java/com/example/demo/jwt/JwtFilter.java
@@ -0,0 +1,52 @@
+package com.example.demo.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
+    private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private TokenProvider tokenProvider;
+
+    public JwtFilter(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+    // dofilter = 토큰의 인증정보를 SecurityContext에 저장하는 역할 수행행
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String jwt = resolveToken(httpServletRequest); // 토큰을 받아와서
+        String requestURI = httpServletRequest.getRequestURI();
+
+        if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)){ // 유효성검증을 하고
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            // 정상토큰이면 authentication 을 받아와서 SecurityContext에 저장
+            logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+        }
+        else {
+            logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+        }
+    }
+
+    // request header에서 토큰 정보를 꺼내오기 위한 메소드
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
